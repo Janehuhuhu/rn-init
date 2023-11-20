1a802ffd3c3ee675f60cf97f18bb1dac1287ca63
Jane <1251756971@qq.com>
2023-11-20 17:29:29 +0800

    feat: 首页

--- src/components/demo/screens/Home.tsx
+++ src/components/demo/screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Text,
   View,
@@ -7,13 +7,110 @@ import {
   ScrollView,
   Dimensions,
   Image,
+  FlatList,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Swiper from 'react-native-swiper';
+import axios from 'axios';
+import LinearGradient from 'react-native-linear-gradient';
+
+const BASE_URL = 'https://devapi.qweather.com/';
+const location = '112.222,39.222';
+const key = '687e517f06684448a9f4695721414a07';
 
 const componentName = () => {
+  const [city, setCity] = useState('中国');
+  const [indices, setIndices] = useState([]);
+  const [weather, setWeather] = useState([]);
+
+  const getWeather = async () => {
+    const res = await axios.get(
+      `${BASE_URL}v7/weather/3d?key=${key}&location=${location}`,
+    );
+    if (res.data?.daily) {
+      setWeather(res.data.daily);
+    }
+    console.log('sss', res.data.daily);
+  };
+
+  const getIndices = async () => {
+    const res = await axios.get(
+      `${BASE_URL}v7/indices/1d?type=1,2,3,4,5&key=${key}&location=${location}`,
+    );
+    if (res.data?.daily) {
+      setIndices(res.data.daily);
+    }
+  };
+
+  const getCity = async () => {
+    const res = await axios.get(
+      `https://geoapi.qweather.com/v2/city/lookup?key=${key}&location=${location}`,
+    );
+    if (res.data?.location?.length) {
+      const data = res.data?.location[0];
+      setCity(data.country + data.adm1 + data.adm2);
+    }
+  };
+
+  const IndicesItem = ({ item, onPress, style }) => {
+    return (
+      <View
+        style={{
+          width: 80,
+          height: 70,
+          borderRadius: 10,
+          backgroundColor: 'rgb(222,238,186)',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginHorizontal: 5,
+        }}>
+        <Text>{item.name}</Text>
+        <Text style={{ color: 'rgb(115,192,137)', marginTop: 5 }}>
+          {item.category}
+        </Text>
+      </View>
+    );
+  };
+
+  const weatherItem = ({ item }) => {
+    return (
+      <View style={{ alignItems: 'center' }}>
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          colors={['#ddd', '#333']}
+          style={{
+            borderRadius: 10,
+            height: 80,
+            width: '95%',
+            marginVertical: 5,
+            alignItems: 'center',
+          }}>
+          <Text style={{ color: 'white', fontSize: 18 }}>{item.fxDate}</Text>
+          <View
+            style={{
+              width: '100%',
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+            }}>
+            <Text style={{ fontSize: 16 }}>
+              {item.textDay}
+              {item.tempMin}°
+            </Text>
+            <Text style={{ fontSize: 16 }}>
+              {item.textNight}
+              {item.tempMax}°
+            </Text>
+          </View>
+        </LinearGradient>
+      </View>
+    );
+  };
+
   useEffect(() => {
-    console.log('sss');
+    getWeather();
+    getIndices();
+    getCity();
   }, []);
 
   return (
@@ -68,6 +165,24 @@ const componentName = () => {
             />
           </View>
         </Swiper>
+      </View>
+      <View style={{ marginVertical: 10, alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{city}</Text>
+      </View>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <FlatList
+          data={indices}
+          keyExtractor={item => item.type}
+          renderItem={IndicesItem}
+          horizontal={true}
+        />
+      </ScrollView>
+      <View>
+        <FlatList
+          data={weather}
+          keyExtractor={item => item.fxDate}
+          renderItem={weatherItem}
+        />
       </View>
     </View>
   );
